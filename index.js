1f0b8e1da1ea7393eeed8cb098ba91d0111b8f50
Tammy200 <tammyagreste2000@gmail.com>
2025-02-06 02:42:27 +0100

    fix(db): resolve MongoDB connection issue

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+require('dotenv').config();
+const PORT = process.env.PORT
 
 app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'My task manager api is working!'
+    })
+});
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}.`);
+});
